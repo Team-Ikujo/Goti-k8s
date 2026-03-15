54cc634c21ce60a0ff4035ffe0a788272033739c
ressKim <ressK4149@gmail.com>
2026-03-16 08:46:09 +0900

    [CHORE] 대시보드 신규 변수 치환 추가 (#38)

--- scripts/validate/extract-queries.sh
+++ scripts/validate/extract-queries.sh
@@ -59,6 +59,15 @@ substitute_vars() {
   expr="${expr//\$\{search\}/}"
   expr="${expr//\$datasource/}"
   expr="${expr//\$\{datasource\}/}"
+  # K8s 운영 변수 (cluster-nodes, pod-lifecycle, capacity-planning 등)
+  expr="${expr//\$namespace/.*}"
+  expr="${expr//\$\{namespace\}/.*}"
+  expr="${expr//\$node/.*}"
+  expr="${expr//\$\{node\}/.*}"
+  expr="${expr//\$workload/.*}"
+  expr="${expr//\$\{workload\}/.*}"
+  expr="${expr//\$deployment/.*}"
+  expr="${expr//\$\{deployment\}/.*}"
   # HTTP 필터 변수 (multi-select, includeAll → .* 치환)
   expr="${expr//\$http_route/.*}"
   expr="${expr//\$\{http_route\}/.*}"
